73f5687ca6422d39d1cb4d891a0557c39c7efd2d
Tryingtroyl <aztec1311@gmail.com>
2022-06-14 16:21:34 +0530

    Deployed S3 Buckets

--- app.py
+++ app.py
@@ -2,27 +2,25 @@
 import os
 
 import aws_cdk as cdk
+from cdk.cdk_stack import HelloCdkStack, AnotherOne
 
-from cdk.cdk_stack import CdkStack
-
-
+# Starts the Python CDK Application
 app = cdk.App()
-CdkStack(app, "CdkStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+"""
+The described stack. Notice that this creates an object of
+the class with the values
+app -> The scope
+"HelloStack" -> Logical ID of the Stack
+env -> A describing variable which creates a CDK environment
+with the account number that is already configured in the
+AWS CLI and the region.
+"""
+HelloCdkStack(app, "HelloStack",
+ env=cdk.Environment(account='434758973508',region='ap-south-1'),)
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+AnotherOne(app, "NewAnother",
+env=cdk.Environment(account='434758973508',region='ap-south-1'),)
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
+# Synthesises the app
 app.synth()
--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -1,19 +1,36 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+import aws_cdk as cdk # Import aws_cdk package which contains all the constructs
+import aws_cdk.aws_s3 as s3 # Import a specific S3 construct for ease
+            
+# A class representing a single stack which inherits from the stack construct
+class HelloCdkStack(cdk.Stack): 
 
-class CdkStack(Stack):
+    """
+    A Python Constructor
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    Every Construct takes 3 arguments, a scope, a logical construct ID and
+    a trailing list of construct arguments (such as Bucket name, versioning, etc.)
+    Here, the constructor takes self, the scope which is this particular app, a 
+    logical id of type str which will be defined later, and zero or more parameters
+    describing the construct.
+    """
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
+
+        # A function call executing the base class Constructor of the Construct
+        # and providing the three arguments mentioned above.
+        super().__init__(scope, construct_id, **kwargs)
+        
+        """
+        This is how we deploy resources.
+
+        self -> The scope of the Construct, i.e. this particular Stack construct
+        "MyFirstBucket" -> The logical id of the construct i.e. the bucket
+        versioned=True -> An argument describing the construct
+        """
+        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True)
+
+class AnotherOne(cdk.Stack): 
+
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(self, "MySecondBucket", versioned=True)
